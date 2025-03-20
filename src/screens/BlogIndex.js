fcba573ee84fd40f60d4269c6616191cc2d66fcb
hoshoo21 <c_gawera@yahoo.com>
2025-03-20 12:50:06 -0700

    initial setup

---
+++ src/screens/BlogIndex.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Text,View, StyleSheet} from 'react-native';
+
+
+const BlogIndex =(props)=>{
+        return (
+            <View>
+                <Text> Blog Index Screen</Text>
+            </View>
+        )
+
+}
+
+
+const styles=  StyleSheet.create({
+
+});
+
+export default BlogIndex;
